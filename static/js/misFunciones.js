05f8c099ac74a40732a28b8abad115aa27a5332d
MarcoSevillaM <marcosevillam@gmail.com>
2024-02-28 20:59:54 +0100

    Justo antes de probar el proyecto en un servidor como Apache o Nginx

---
+++ static/js/misFunciones.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Agrega un evento de clic a cada fila
+  var filas = document.querySelectorAll('tr[data-href]');
+  filas.forEach(function (fila) {
+    fila.addEventListener('click', function () {
+        var url = fila.getAttribute('data-href');
+        if (url) {
+          window.location.href = url;
+        }
+    });
+  });
+});
+function mostrarIndicadorDeCarga(formId) {
+  // Obtener el formulario y el botón
+  var form = document.getElementById(formId);
+  var button = form.querySelector('button');
+
+  // Deshabilitar el botón y cambiar el texto
+  button.disabled = true;
+  button.innerHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Cargando...';
+
+  // Aquí puedes agregar más lógica de carga si es necesario
+
+  // Enviar el formulario después de un breve retraso (puedes ajustar el tiempo)
+  setTimeout(function() {
+    form.submit();
+  }, 500);
+}
